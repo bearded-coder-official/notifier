a48190df4d052ecdd328d6b243a84516c17fab6c
Stanislav Protasevich <stanislav.protasevich@gmail.com>
2018-12-14 19:20:16 +0200

    Инициализация проекта

---
+++ src/NotifierAdapterInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace NtSchool\Notifier;
+
+interface NotifierAdapterInterface
+{
+    public function debug(string $message);
+
+    public function info(string $message);
+
+    public function notice(string $message);
+
+    public function warning(string $message);
+
+    public function error(string $message);
+
+    public function critical(string $message);
+
+    public function alert(string $message);
+
+    public function emergency(string $message);
+}
